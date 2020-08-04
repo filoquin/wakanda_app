b13f41744ef2e510c877ff88e8a985ccfe0009f1
seba <sebag.torres@gmail.com>
2020-08-04 07:42:29 -0300

    fix merge

--- src/app/_services/sale-order.service.ts
+++ src/app/_services/sale-order.service.ts
@@ -1,25 +1,26 @@
-import { Injectable } from "@angular/core";
-import { OdooRPCService } from "./odoorcp.service";
+import {Injectable} from '@angular/core';
+import {OdooRPCService} from './odoorcp.service';
 
 @Injectable({
-	providedIn: "root",
+  providedIn: 'root',
 })
 export class SaleOrderService {
-	constructor(public odooRPC: OdooRPCService) {}
+  constructor(public odooRPC: OdooRPCService) {
+  }
 
-	getCategories() {
-		return this.odooRPC.searchRead(
-			"product.category",
-			[["wkn_publish", "=", true]],
-			["name", "display_name"]
-		);
-	}
-
-	getProducts(categoryId) {
+  getCategories() {
     return this.odooRPC.searchRead(
-      "product.template",
-      [["wak_published", "=", true], ["categ_id", "child_of", categoryId]],
-      ["default_code", "name", "display_name","qty_available","list_price"]
+      'product.category',
+      [['wkn_publish', '=', true]],
+      ['name', 'display_name']
     );
-	}
+  }
+
+  getProducts(categoryId) {
+    return this.odooRPC.searchRead(
+      'product.template',
+      [['wak_published', '=', true], ['categ_id', 'child_of', categoryId]],
+      ['default_code', 'name', 'display_name', 'qty_available', 'list_price', 'price']
+    );
+  }
 }
