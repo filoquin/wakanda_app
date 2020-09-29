8a21f449e2cdeab95d7b18a5463662247e13777a
filoquin <filoquin@gmail.com>
2020-09-29 10:31:00 -0300

    back to authguard

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-//import {UserService} from '../_services/user.service';
-import { OdooRPCService } from "../_services/odoorcp.service";
-import { Observable } from "rxjs";
+import {UserService} from '../_services/user.service';
+
+
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(
     private router: Router,
-    //private userService: UserService,
-       private odooRPC: OdooRPCService 
+    private userService: UserService
   ) {}
 
-  /*canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const user = this.userService.userValue;
     if (user) {
       // authorised so return true
@@ -22,23 +21,5 @@ export class AuthGuard implements CanActivate {
     // not logged in so redirect to login page with the return url
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
     return false;
-  }*/
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
-  ): Observable<boolean> | Promise<boolean> | boolean {
-    return new Observable<boolean>((obs) => {
-      this.odooRPC.getSessionInfo().then((result: any) => {
-        if (result.uid) {
-          obs.next(true);
-        } else {
-          this.router.navigate(["/login"], {
-            queryParams: { returnUrl: state.url },
-          });
-
-          obs.next(false);
-        }
-      });
-    });
   }
 }
