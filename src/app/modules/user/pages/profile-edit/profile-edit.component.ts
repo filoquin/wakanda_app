51988b8b98f23670dfbd7988d5f1dcb5b7642f9d
seba <sebag.torres@gmail.com>
2020-09-28 21:55:41 -0300

    Was updated profile edit form

--- src/app/modules/user/pages/profile-edit/profile-edit.component.ts
+++ src/app/modules/user/pages/profile-edit/profile-edit.component.ts
@@ -12,17 +12,18 @@ export class ProfileEditComponent implements OnInit {
   editForm: FormGroup;
   isSubmitted = false;
   partnerId: number;
+  public image:any;
   constructor(
     private userService: UserService,
     private router: Router,
     private formBuilder: FormBuilder
-  ) {}
-  actual_year(){
-      return new Date().getFullYear()
+  ) { }
+  actual_year() {
+    return new Date().getFullYear()
 
   }
   counter(i: number) {
-      return new Array(i);
+    return new Array(i);
   }
   ngOnInit(): void {
     this.editForm = this.formBuilder.group({
@@ -68,7 +69,7 @@ export class ProfileEditComponent implements OnInit {
     const email = this.editForm.controls.email.value;
     const phone = this.editForm.controls.phone.value;
     const street = this.editForm.controls.address.value;
-    const birthdate = this.editForm.controls.year.value + "-" + ("00" + this.editForm.controls.month.value).slice(-2) + "-" +this.editForm.controls.day.value ;
+    const birthdate = this.editForm.controls.year.value + "-" + ("00" + this.editForm.controls.month.value).slice(-2) + "-" + this.editForm.controls.day.value;
 
     this.userService
       .saveProfile(this.partnerId, name, email, phone, street, birthdate)
@@ -80,4 +81,20 @@ export class ProfileEditComponent implements OnInit {
         console.log("errors" + err);
       });
   }
+
+  fileChange(input){
+    const reader = new FileReader();
+    if (input.files.length) {
+        const file = input.files[0];
+        reader.onload = () => {
+            this.image = reader.result;
+        }
+        reader.readAsDataURL(file);
+    }
+}
+
+removeImage():void{
+    this.image = '';
+}
+
 }
